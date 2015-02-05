7e55f697a6294268ed785698034c976adbe5f927
Matt <navx2810@gmail.com>
2015-02-04 21:48:03 -0500

    added the new UI and created the GridSizeWithAnchor script which will resize the cell size depending on the parent's width.

---
+++ Assets/Scripts/UI/GridSizeWithAnchor.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GridSizeWithAnchor : MonoBehaviour
+{
+		public RectTransform rT;
+		public GridLayoutGroup gLG;
+		public float width;
+		
+		
+		// Use this for initialization
+		void Start ()
+		{
+				rT = GetComponent<RectTransform> ();
+				gLG = GetComponent<GridLayoutGroup> ();
+				gLG.cellSize = new Vector2 (rT.rect.width, gLG.cellSize.y);
+
+		}
+	
+		void OnRectTransformDimensionsChange ()
+		{
+				try {
+						gLG.cellSize = new Vector2 (rT.rect.width, gLG.cellSize.y);
+				} catch (UnassignedReferenceException uRE) {
+				}
+		}
+
+}
