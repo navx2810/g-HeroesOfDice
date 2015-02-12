2b5187fca8fe66e424c0dd6344ed4b35fa9809cb
Matt <navx2810@gmail.com>
2015-02-12 18:37:17 -0500

    added the ability bars grid size script

---
+++ Assets/Scripts/UI/GridSizeWithRatio.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GridSizeWithRatio : MonoBehaviour
+{
+		GridLayoutGroup gridLayoutGroup;
+		RectTransform rect;
+		public float height;
+		public int cellCount = 2;
+		// Use this for initialization
+		void Start ()
+		{
+				gridLayoutGroup = GetComponent<GridLayoutGroup> ();
+				rect = GetComponent<RectTransform> ();
+				
+				
+				gridLayoutGroup.cellSize = new Vector2 (rect.rect.height, rect.rect.height);
+				cellCount = GetComponentsInChildren<RectTransform> ().Length;
+				
+		}
+
+		void OnRectTransformDimensionsChange ()
+		{
+				if ((rect.rect.height + (gridLayoutGroup.padding.horizontal * 2)) * cellCount < rect.rect.width)
+						gridLayoutGroup.cellSize = new Vector2 (rect.rect.height, rect.rect.height);
+		}
+}
