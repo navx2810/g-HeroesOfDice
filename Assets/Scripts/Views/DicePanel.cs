78e6dd3168467ddb3264875c53937b1856fc523f
Matt <navx2810@gmail.com>
2015-02-26 10:12:51 -0500

    updated the panels and moved the panels

---
+++ Assets/Scripts/Views/DicePanel.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DicePanel : MonoBehaviour
+{
+
+		public EntityPanel[] entityPanel;
+
+		// Use this for initialization
+		void Start ()
+		{
+		
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+	
+		}
+
+		public void Init (DiceModel[] diceModel)
+		{
+				for (int x = 0; x < diceModel.Length; x++)
+						entityPanel [x].Init (diceModel [x].name);
+				// This makes the assumption there's always an equal amount of panels to models
+		}
+}
