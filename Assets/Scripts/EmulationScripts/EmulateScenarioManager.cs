f1e7c7bc3227e5ce5b8582d685951e846c2775cd
Matt Fetterman <navx2810@gmail.com>
2015-04-17 11:19:41 -0400

    Added an emulation script

---
+++ Assets/Scripts/EmulationScripts/EmulateScenarioManager.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using System.Collections;
+using HeroesOfDice;
+using HeroesOfDice.Managers;
+
+public class EmulateScenarioManager : MonoBehaviour {
+
+    void Awake()
+    {
+
+        BDice[] party = new BDice[3];
+        party[0] = new Warrior("Max", 10, ETargetType.Ally);
+        party[1] = new Cleric("Jason", 10, ETargetType.Ally);
+        party[2] = new Sorcerer("Mike", 6, ETargetType.Ally);
+
+        BDice[] enemy = new BDice[3];
+        enemy[0] = new Warrior("Clara", 10, ETargetType.Enemy);
+        enemy[1] = new Cleric("Mich", 8, ETargetType.Enemy);
+        enemy[2] = new Sorcerer("Veela", 7, ETargetType.Enemy);
+
+        party[0].CurrentHealth = 5;
+
+        CombatManager.Instance.PlayersParty = party;
+        CombatManager.Instance.EnemyParty = enemy;
+
+        MenuManager.Instance.CurrentState = MenuManager.Instance.States[0];
+    }
+    
+}
