1f0d1a2000036d3de34b2115ffba4434175e7319
Matt Fetterman <navx2810@gmail.com>
2015-04-18 10:04:49 -0400

    added the event functions for handling when an ability is registered

--- Assets/Scripts/Views/AbilityPanel.cs
+++ Assets/Scripts/Views/AbilityPanel.cs
@@ -1,3 +1,5 @@
+using HeroesOfDice;
+using HeroesOfDice.Managers;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -5,15 +7,46 @@ public class AbilityPanel : MonoBehaviour
 {
 
     public AbilityButton[] buttons;
+    
 
-		// Use this for initialization
-		void Start ()
-		{
-                //abilityButtons = GetComponentInChildren<RectTransform> ().GetComponentsInChildren<Button> ();
-		    buttons = GetComponentInChildren<RectTransform>().GetComponentsInChildren<AbilityButton>();
-		}
-
-    public void UpdateView()
+    // Use this for initialization
+    void Start()
     {
+        //abilityButtons = GetComponentInChildren<RectTransform> ().GetComponentsInChildren<Button> ();
+        buttons = GetComponentInChildren<RectTransform>().GetComponentsInChildren<AbilityButton>();
     }
+
+    public void OnEnable()
+    {
+        CombatManager.Instance.OnAbilityRegister += AddToView;
+        CombatManager.Instance.OnAbilityUnregister += RemoveFromView;
+    }
+
+    public void OnDisable()
+    {
+        CombatManager.Instance.OnAbilityRegister -= AddToView;
+        CombatManager.Instance.OnAbilityUnregister -= RemoveFromView;
+    }
+
+    public void AddToView(BDice dice)
+    {
+        for(int x = 0; x < buttons.Length; x++)
+            if (buttons[x].model == null)
+            {
+                buttons[x].SetModel(dice);
+                break;
+            }
+    }
+
+    public void RemoveFromView(BDice dice)
+    {
+        for(int x = 0; x < buttons.Length; x++)
+            if (buttons[x].model == dice)
+            {
+                buttons[x].Flush();
+                break;
+            }
+    }
+
+
 }
