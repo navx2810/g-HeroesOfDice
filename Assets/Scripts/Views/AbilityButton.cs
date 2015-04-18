f1c8b4049b82c81864986043bc81f767322fc01a
Matt Fetterman <navx2810@gmail.com>
2015-04-18 10:01:09 -0400

    added a temporary Text object for the name of the ability, added a method to flush the ability object

--- Assets/Scripts/Views/AbilityButton.cs
+++ Assets/Scripts/Views/AbilityButton.cs
@@ -7,4 +7,16 @@ public class AbilityButton : MonoBehaviour
 {
     public BDice model;
     public Button button;
+    public Text abilityName;
+
+    public void Flush()
+    {
+        model = null;
+    }
+
+    public void SetModel(BDice model)
+    {
+        this.model = model;
+        abilityName.text = model.UpSide.Ability.Name;
+    }
 }
