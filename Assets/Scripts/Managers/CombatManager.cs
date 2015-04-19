c80a74eaf115928d2b058d326823e3615c6c12e3
Matt Fetterman <navx2810@gmail.com>
2015-04-19 13:20:41 -0400

    added two dictionaries for the player and enemy abilities

--- Assets/Scripts/Managers/CombatManager.cs
+++ Assets/Scripts/Managers/CombatManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using HeroesOfDice.GameObjects;
 using UnityEngine;
@@ -32,22 +33,23 @@ namespace HeroesOfDice.Managers
                 if (this != _instance)
                     Destroy(gameObject);
 
-            HasAbility = new List<BDice>(6);
+            PlayerAbilities = new Dictionary<BDice, BAbility>(3);
+            EnemyAbilities = new Dictionary<BDice, BAbility>(3);
         }
 
         public BDice Attacker { get; set; }
         public BDice Defender { get; set; }
         public BDice[] PlayersParty { get; set; }
         public BDice[] EnemyParty { get; set; }
-            
-         [SerializeField]
+
+        [SerializeField]
         private Dice[] _partyDice;
         public Dice[] PartyDiceObjects
         {
             get { return _partyDice; }
             set { _partyDice = value; }
         }
-        
+
         [SerializeField]
         private Dice[] _enemyDice;
         public Dice[] EnemyDiceObjects
@@ -56,7 +58,8 @@ namespace HeroesOfDice.Managers
             set { _enemyDice = value; }
         }
 
-        public List<BDice> HasAbility { get; set; }
+        public Dictionary<BDice, BAbility> PlayerAbilities { get; set; }
+        public Dictionary<BDice, BAbility> EnemyAbilities { get; set; }
 
         public void ConfirmSelection()
         {
@@ -68,25 +71,40 @@ namespace HeroesOfDice.Managers
 
         public void RegisterAbility(BDice dice)
         {
-            HasAbility.Add(dice);
-            OnAbilityRegister(dice);
+            if (dice.UpSide.Ability.TargetType != ETargetType.None)
+                if (dice.TargetType == ETargetType.Ally)
+                    PlayerAbilities.Add(dice, dice.UpSide.Ability);
+                else
+                    EnemyAbilities.Add(dice, dice.UpSide.Ability);
+            
+            if (OnAbilityRegister != null) OnAbilityRegister(dice);
 
             // TODO: add check to see if all abilities do not have the target type of None, otherwise the turn would end
-            if (HasAbility.Count == PlayersParty.Length)
+            if (PlayerAbilities.Count == PlayersParty.Length)
             {
                 bool allNone = true;
-                foreach (var d in HasAbility)
-                    if (d.UpSide.Ability.TargetType != ETargetType.None)
-                        allNone = false;
-                if (allNone) { }
-                    //End turn
+                foreach (var d in PlayerAbilities)
+                    if (d.Key.TargetType == ETargetType.Ally)
+                        if (d.Value.TargetType != ETargetType.None)
+                            allNone = false;
+                if (allNone)
+                    Debug.Log("No usable abilities, ending turn");
             }
         }
 
         public void UnregisterAbility(BDice dice)
         {
-            HasAbility.Remove(dice);
-            OnAbilityUnregister(dice);
+            PlayerAbilities.Remove(dice);
+            if (OnAbilityUnregister != null) OnAbilityUnregister(dice);
+
+            Debug.Log("[--- What is left over ---]");
+            foreach (var key in PlayerAbilities)
+                Debug.Log(String.Format("{0} : {1}", key.Key.Name, key.Value.Name));
+
+            // Problem here is that dice does not look to see who is an ally or not
+
+            if (PlayerAbilities.Count == 0)
+                Debug.Log("Turn over");
         }
 
         public delegate void NotifyChange(BDice dice);
