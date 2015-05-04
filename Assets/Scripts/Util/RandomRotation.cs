2f7e45fa9ba58ad7338fb088578ae25f938414b4
Matt Fetterman <navx2810@gmail.com>
2015-05-04 17:06:32 -0400

    added the new method of random rotation

--- Assets/Scripts/Util/RandomRotation.cs
+++ Assets/Scripts/Util/RandomRotation.cs
@@ -4,10 +4,15 @@ namespace HeroesOfDice.Util
 {
     public class RandomRotation : MonoBehaviour
     {
+
+        public Vector3 ForwardVector;
+
         public void OnEnable()
         {
+            var rb = GetComponent<Rigidbody>();
             transform.rotation = Quaternion.Euler(Random.Range(0, 360f), Random.Range(0, 360f), Random.Range(0, 360f));
-            GetComponent<Rigidbody>().AddTorque(new Vector3(Random.Range(0, 360f), Random.Range(0, 360f), Random.Range(0, 360f)) * 200f);
+            rb.AddTorque(new Vector3(Random.Range(0, 360f), Random.Range(0, 360f), Random.Range(0, 360f)) * 200f);
+            rb.AddForce(ForwardVector * 100);
             enabled = false;
         }
     }
