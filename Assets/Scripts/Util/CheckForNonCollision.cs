01395faf0798e6bbb0314f87e98f8cf712f561b4
Matt Fetterman <navx2810@gmail.com>
2015-05-04 17:06:20 -0400

    changed the foreach to a for loop to retrieve the index, added the tweens and a reference to a RestPosition

--- Assets/Scripts/Util/CheckForNonCollision.cs
+++ Assets/Scripts/Util/CheckForNonCollision.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using HeroesOfDice.GameObjects;
 using HeroesOfDice.Managers;
 using UnityEngine;
@@ -7,7 +8,7 @@ namespace HeroesOfDice.Util
     [RequireComponent(typeof(Rigidbody))]
     public class CheckForNonCollision : MonoBehaviour
     {
-        public Transform _tf;
+        public Transform _tf, RestPosition;
         public Vector3 oldPosition, newPosition, oldRotation, newRotation;
         public Dice dice;
 
@@ -36,21 +37,49 @@ namespace HeroesOfDice.Util
 
         void CheckSides()
         {
-            foreach (DiceSide side in dice.sides)
-                if (Vector3.Dot(side.transform.forward, dice.floor.up) > .95f)
+            //foreach (DiceSide side in dice.sides)
+            //    if (Vector3.Dot(side.transform.forward, dice.floor.up) > .95f)
+            //    {
+            //        dice.upSide = side;
+            //        MoveToRestPosition();
+            //        break;
+            //    }
+
+            for (var x = 0; x < dice.sides.Length; x++)
+                if (Vector3.Dot(dice.sides[x].transform.forward, dice.floor.up) > .95f)
                 {
-                    dice.upSide = side;
+                    dice.upSide = dice.sides[x];                            // Dice sides are not really needed
+                    dice.diceModel.UpSide = dice.diceModel.Abilities[x];
+                    MoveToRestPosition();
                     break;
                 }
 
             if (dice.upSide == null)
                 dice.ShootUp();
-            else
-            {
-                dice.diceModel.UpSide = dice.diceModel.Abilities[dice.upSide.Index];
-                CombatManager.Instance.RegisterAbility(dice.diceModel);
-                enabled = false;
-            }
+            //else
+            //{
+            //    dice.diceModel.UpSide = dice.diceModel.Abilities[dice.upSide.Index];
+            //    CombatManager.Instance.RegisterAbility(dice.diceModel);
+            //    enabled = false;
+            //}
+        }
+
+        private void MoveToRestPosition()
+        {
+            GetComponent<BoxCollider>().isTrigger = true;
+            GetComponent<Rigidbody>().isKinematic = true;
+            transform.DOMove(RestPosition.position, 1f).OnComplete(OnRestPosition);
+            enabled = false;
+        }
+
+        private void OnRestPosition()
+        {
+            GetComponent<BoxCollider>().isTrigger = false;
+            GetComponent<Rigidbody>().isKinematic = false;      // Turn this off before tweening so it won't fall during movement
+
+            // Register that dice has ability with combat manager
+            CombatManager.Instance.RegisterAbility(dice.diceModel);
         }
     }
+
 }
