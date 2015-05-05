5dd4e74390d4dc3a11e1c5e51398bf41d0b70539
Matt Fetterman <navx2810@gmail.com>
2015-05-04 22:43:10 -0400

    added new tweens for healing yourself or healing an ally

--- Assets/Scripts/Views/States/BattleTweenScreen.cs
+++ Assets/Scripts/Views/States/BattleTweenScreen.cs
@@ -9,7 +9,7 @@ using HeroesOfDice.Util;
 public class BattleTweenScreen : BMenuState
 {
     public GameObject TweenCatcher;
-    public Transform AttackerPosition, DefenderPosition, Attacker, Defender, AttackersRest, DefendersRest;
+    public Transform AttackerPosition, DefenderPosition, SinglePosition, Attacker, Defender, AttackersRest, DefendersRest;
     public Rigidbody AttackersRB, DefendersRB;
     public BoxCollider AttackersBC, DefendersBC;
 
@@ -36,14 +36,31 @@ public class BattleTweenScreen : BMenuState
         AttackersRest = Attacker.GetComponent<CheckForNonCollision>().RestPosition;
         DefendersRest = Defender.GetComponent<CheckForNonCollision>().RestPosition;
 
-        Attacker.DOMove(AttackerPosition.position, .5f);
-        Defender.DOMove(DefenderPosition.position, .5f).OnComplete(StartAttack);
+        if (Attacker.Equals(Defender))
+            Attacker.DOMove(SinglePosition.position, .5f).OnComplete(HandleSelfHeal);
+        else if (CombatManager.Instance.Attacker.UpSide.Ability.TargetType == ETargetType.Enemy)
+        {
+            Attacker.DOMove(AttackerPosition.position, .5f);
+            Defender.DOMove(DefenderPosition.position, .5f).OnComplete(StartAttack);
+        }
+        else
+        {
+            Attacker.DOMove(AttackerPosition.position, .5f);
+            Defender.DOMove(DefenderPosition.position, .5f).OnComplete(StartHeal); 
+        }
     }
 
     private void StartAttack()
     {
         attackerTempPos = Attacker.position;
-        Attacker.DOMove(Defender.position, .1f).OnComplete(HandleAttack);
+        Attacker.DOMove(Defender.position, .1f).SetEase(Ease.InOutBounce).OnComplete(HandleAttack);
+    }
+
+    private void StartHeal()
+    {
+        attackerTempPos = Attacker.position;
+        Attacker.DOMove(Attacker.position + AttackerPosition.up*1.5f, .2f).SetEase(Ease.InExpo).OnComplete(HandleHeal);
+    
     }
 
     private void HandleAttack()
@@ -51,8 +68,24 @@ public class BattleTweenScreen : BMenuState
         // TODO: Eventually, handle damaging the player here, but dont let the turn end until the animation is complete
         //CombatManager.Instance.Attacker.UseAbility();         
         Defender.DOShakeRotation(1f, 50).OnComplete(MoveBack);
-        Attacker.DOMove(attackerTempPos, .2f);
+        Attacker.DOMove(attackerTempPos, .2f).SetEase(Ease.InOutBack);
 
+    }
+
+    private void HandleSelfHeal()
+    {
+        Attacker.DOPunchRotation(AttackerPosition.up * 25f, .5f).OnComplete(MoveSingleBack);
+    }
+
+    private void HandleHeal()
+    {
+        Attacker.DOMove(attackerTempPos, .5f).SetEase(Ease.OutExpo);
+        Defender.DOPunchRotation(DefenderPosition.up * 25f, .5f).OnComplete(MoveBack);
+    }
+
+    private void MoveSingleBack()
+    {
+        Attacker.DOMove(AttackersRest.position, .5f).OnComplete(MenuManager.Instance.CallGameScreen);
     }
 
     private void MoveBack()
