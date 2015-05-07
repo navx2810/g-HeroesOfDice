77e6394d39dd1a2a24e90f3efa1d093afd4b2b0d
Matt Fetterman <navx2810@gmail.com>
2015-05-07 19:50:23 -0400

    added a check when the enemies turn is over to see if abilities are still left, this is a hack and definitely shouldn't be how the turn works, the coroutine should continue until no abilities remain, but it does not

--- Assets/Scripts/Managers/TurnManager.cs
+++ Assets/Scripts/Managers/TurnManager.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using System.Collections;
 
@@ -42,18 +43,33 @@ namespace HeroesOfDice.Managers
         {
             if (CombatManager.Instance.IsGameOver)
                 return;
+
             if (!IsPlayersTurn)
             {
-                Debug.Log("Enemies Turn over, Rerolling");
-                Roll();
-                if (TurnEnding != null) TurnEnding();
-                MenuManager.Instance.DisplayMessage("Enemies turn ending", 2f);
-                CombatManager.Instance.PlayerAbilities.Clear();
-                CombatManager.Instance.EnemyAbilities.Clear();
-                IsPlayersTurn = true;
+                if (CombatManager.Instance.EnemyAbilities.Keys.Count > 0)
+                {
+                    foreach (var d in CombatManager.Instance.EnemyAbilities.Keys)
+                        Debug.LogErrorFormat("{0} still has ability: {1}, target type of {2}", d.Name,
+                            d.UpSide.Ability.Name, d.UpSide.Ability.TargetType);
+                    //throw new Exception("You done fucked up, There shouldn't be abilities left");
+                    AI.StopAllCoroutines();
+                    AI.IsPerforming = false;
+                    AI.DoAction();
+                }
+                else
+                {
+                    AI.StopAllCoroutines();
+                    Roll();
+                    if (TurnEnding != null) TurnEnding();
+                    MenuManager.Instance.DisplayMessage("Enemies turn ending", 2f);
+                    CombatManager.Instance.PlayerAbilities.Clear();
+                    CombatManager.Instance.EnemyAbilities.Clear();
+                    IsPlayersTurn = true;
+                }
             }
             else
             {
+
                 MenuManager.Instance.DisplayMessage("Player's turn ending", 2f);
                 IsPlayersTurn = false;
                 AI.DoAction();
